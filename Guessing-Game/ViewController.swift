85009493be6ebc957a21095c8eadc818857472ef
Emily Baker-King <emilybaker-king@Interapts-MacBook-Pro-102.local>
2018-09-21 10:56:47 -0500

    added the rest of the files

---
+++ Guessing-Game/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Guessing-Game
+//
+//  Created by Emily Baker-King on 9/21/18.
+//  Copyright © 2018 Emily Baker-King. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
