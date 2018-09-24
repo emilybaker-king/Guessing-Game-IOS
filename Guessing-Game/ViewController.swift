3e34fe75a497998b0135e77519a193ccf97ff9e5
Emily Baker-King <emilybaker-king@interapts-macbook-pro-102.local>
2018-09-24 13:46:14 -0500

    beginning

--- Guessing-Game/ViewController.swift
+++ Guessing-Game/ViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var attemps: UILabel!
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var greetingLabel: UILabel!
+    @IBOutlet weak var guessStatusLabel: UILabel!
+    @IBOutlet weak var submitButton: UIButton!
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        greetingLabel.text = "Pick a number between 1-100"
     }
 
+    @IBAction func resetClick(_ sender: Any) {
+        
+    }
+    
+    @IBAction func submitClick(_ sender: Any) {
+        
+    }
+    
 
 }
 
